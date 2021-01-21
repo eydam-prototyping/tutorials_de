86a981363ef33aa99b76dff14ac2b4f2c0784eed
Tobias <eydam-prototyping@outlook.com>
2021-01-21 15:16:17 +0100

    setup.py angepasst

--- micropython/packages/ep_logging/setup.py
+++ micropython/packages/ep_logging/setup.py
@@ -6,19 +6,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="micropython_eydam-prototyping_logging",
-    version="0.0b0",
+    version="0.0b3",
     author="Tobias Eydam",
     author_email="eydam-prototyping@outlook.com",
     description="Some logging functions for MicroPython in combination with syslog-server",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/eydam-prototyping/tutorials_de",
-    packages=setuptools.find_packages(),
+    url="https://github.com/eydam-prototyping/tutorials_de/blob/master/micropython/packages/ep_logging",
     classifiers=[
         "Programming Language :: Python :: Implementation :: MicroPython",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    cmdclass={'sdist': sdist_upip.sdist}
+    cmdclass={'sdist': sdist_upip.sdist},
+    py_modules=['ep_logging']
 )
