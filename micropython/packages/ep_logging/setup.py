b979831123aa6b4c13c0f757e8951aff69c59186
Tobias <eydam-prototyping@outlook.com>
2021-01-22 21:19:44 +0100

    Ordnerstruktur geändert

--- micropython/packages/ep_logging/setup.py
+++ micropython/packages/ep_logging/setup.py
@@ -1,12 +1,17 @@
-import setuptools
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
 import sdist_upip
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="micropython_eydam-prototyping_logging",
-    version="0.0b3",
+    version="0.0.6",
     author="Tobias Eydam",
     author_email="eydam-prototyping@outlook.com",
     description="Some logging functions for MicroPython in combination with syslog-server",
